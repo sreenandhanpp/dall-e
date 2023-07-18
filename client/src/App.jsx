3b55b36b46d127a68d161a96ea040e8463653506
sreenandhanpp <sreedeviv27801@gmail.com>
2023-07-18 21:28:57 +0530

    home-page-updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route , Routes, Link } from 'react-router-dom';
 import { logo } from './assets'
 import { CreatePost, Home } from './pages';
 
