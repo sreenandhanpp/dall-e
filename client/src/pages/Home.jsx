68e1bbc8c0260e0ecca957ca0308ae84c9e2407c
sreenandhanpp <sreedeviv27801@gmail.com>
2023-07-24 20:58:16 +0530

    search-feature-updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Card, Loader, FormField } from '../components'
 
 const RenderCard = ({ data, title }) => {
-  if (data?.length > 0){
+  if (data?.length > 0) {
     return data.map((post) => <Card key={post._id} {...post} />)
   }
 
@@ -15,30 +15,47 @@ const RenderCard = ({ data, title }) => {
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
-  const [allPost, setAllPost] = useState([  ]);
+  const [allPost, setAllPost] = useState([]);
   const [searchText, setSearchText] = useState('');
-  useEffect(()=>{
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
-      try{
-        const response = await fetch('http://localhost:8080/api/v1/post',{
-          method:'GET',
-          header:{
-            'Content-Type':'application/json'
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'GET',
+          header: {
+            'Content-Type': 'application/json'
           }
         })
-        if(response.ok){
+        if (response.ok) {
           const result = await response.json();
           setAllPost(result.data.reverse())
         }
-      }catch(err){
+      } catch (err) {
         alert(err)
-      }finally{
+      } finally {
         setLoading(false)
       }
     }
     fetchPosts();
-  },[])
+  }, [])
+
+  const handleSearchText = (e) => {
+    clearTimeout(searchTimeout)
+
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = allPost.filter((item) =>
+          item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+          item.prompt.toLowerCase().includes(searchText.toLowerCase()))
+        setSearchedResults(searchResults)
+      }, 500)
+    )
+  }
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -51,7 +68,14 @@ const Home = () => {
       </div>
 
       <div className='mt-16'>
-        <FormField />
+        <FormField 
+          type="text"
+          labelName="Search posts"
+          name="text"
+          placeholder="Search posts"
+          value={searchText}
+          handleChange={handleSearchText}
+        />
       </div>
 
       <div className='mt-10'>
@@ -71,17 +95,17 @@ const Home = () => {
               }
               <div className='grid lg:grid-cols-4 sm:grid-cols-3 xm:grid-cols-2 grid-cols-1 gap-3'>
                 {searchText ? (
-                  <RenderCard 
-                    data = { [] }
-                    title = "No Search result found"
-                    />
-                ):(
-                  <RenderCard 
-                    data = {allPost}
-                    title= "No Post"
-                    />
+                  <RenderCard
+                    data={searchedResults}
+                    title="No search results found"
+                  />
+                ) : (
+                  <RenderCard
+                    data={allPost}
+                    title="No Post"
+                  />
                 )
-              }
+                }
               </div>
             </>
           )
